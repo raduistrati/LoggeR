7149407a2b63a21c2e85c4ac4dce47c5f0e6621e
Radu Istrati <radonciki@gmail.com>
2022-05-12 22:16:28 +0300

    static shared instance

--- Sources/LoggeR/LoggeR.swift
+++ Sources/LoggeR/LoggeR.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class LoggeR {
   private init() {}
-  public let shared = LoggeR()
+  public static let shared = LoggeR()
   
   public func log(_ message: String) {
     debugPrint(message)
